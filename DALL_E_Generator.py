2882d2e35e6f284d4af6188d5955bdb7379823ba
lanceu0128 <lanceu0128@gmail.com>
2023-01-23 12:14:48 -0500

    Image generator exception handling

--- DALL_E_Generator.py
+++ DALL_E_Generator.py
@@ -1,18 +1,26 @@
 import openai
 import urllib.request
 import os
+import numpy as np
+import time
 
 image_path_album_covers = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images/album-covers")
 
 openai.api_key = ""
 
 def getImage(song):
-    response = openai.Image.create(
-        prompt="Create a unique album cover for a song called {} that is 500 x 500 pixels. ".format(song),
-        n=1,
-        size="1024x1024"
-    )
-    image_url = response['data'][0]['url']
-    urllib.request.urlretrieve(image_url, os.path.join(image_path_album_covers, "{}.jpg".format(song)))
-
+    time.sleep(np.random.uniform(4, 6))
+    try:
+        response = openai.Image.create(
+            prompt="Create a unique album cover for a song called {} that is 500 x 500 pixels. ".format(song),
+            n=1,
+            size="1024x1024"
+        )
+        image_url = response['data'][0]['url']
+        urllib.request.urlretrieve(image_url, os.path.join(image_path_album_covers, "{}.jpg".format(song)))
+    except:
+        print("Exception occured for song {}".format(song), restarting)
+        getImage(song)
+    
+    print("Image for {} successfuly generated".format(song))    
     return "{}.jpg".format(song)
